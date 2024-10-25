4d6523c54c840c123b849d78fef55f10db9b4edb
Mel <eaaalov@students.eaaa.dk>
2024-10-25 11:34:12 +0200

    nugets

--- obj/Debug/net8.0/DeliveryService.AssemblyInfo.cs
+++ obj/Debug/net8.0/DeliveryService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeliveryService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d6203dbb09f7dec82089223a3c143f2d2f4aa4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2196229335935bce0594299db88938e223ee7d24")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeliveryService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeliveryService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

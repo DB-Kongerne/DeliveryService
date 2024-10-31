1ef48469d08a9ce28e7821ff44537716ae5d649a
Mel <eaaalov@students.eaaa.dk>
2024-10-31 11:59:13 +0100

    hej

--- obj/Debug/net8.0/DeliveryService.AssemblyInfo.cs
+++ obj/Debug/net8.0/DeliveryService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeliveryService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+712ad58618eb84c444aeabd696b970c7703dbb4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+621f7f395a60f8b303193e0d4c3a9068c3071871")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeliveryService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeliveryService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
